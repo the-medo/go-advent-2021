0c4f6c210a0db177b6116feb4b68d8b97f907561
Medo <martinmederly@gmail.com>
2023-10-11 00:54:08 +0200

    feat: day 11

--- day11/day11.go
+++ day11/day11.go
@@ -1,5 +1,79 @@
 package day11
 
-func Solve(input string) {
+import (
+	"fmt"
+	"github.com/the-medo/go-advent-2021/t"
+	"github.com/the-medo/go-advent-2021/utils"
+)
 
+func Solve(input string) {
+	matrix, _ := utils.SplitToMatrix(input, false, false)
+
+	totalFlashCount := 0
+	part1 := 0
+	part2 := 0
+	flashes := 0
+
+	for i := 1; flashes < 100; i++ {
+		flashes = processStep(matrix)
+		totalFlashCount += flashes
+		if i == 100 {
+			part1 = totalFlashCount
+		}
+		if flashes == 100 {
+			part2 = i
+			break
+		}
+	}
+
+	fmt.Println("First part: ", part1)
+	fmt.Println("Second part: ", part2)
+
+}
+
+func processStep(matrix [][]int) int {
+	var pointsToExplode []t.Point2D
+
+	for row, matrixRow := range matrix {
+		for col, _ := range matrixRow {
+			matrix[row][col]++
+			if matrix[row][col] == 10 {
+				matrix[row][col] = 0
+				pointsToExplode = append(pointsToExplode, t.Point2D{X: col, Y: row})
+			}
+		}
+	}
+
+	explodedPointCount := explodePoints(matrix, pointsToExplode)
+
+	return explodedPointCount
+}
+
+func explodePoints(matrix [][]int, pointsToExplode []t.Point2D) (explodedPointCount int) {
+	width, height := len(matrix[0]), len(matrix)
+
+	explodedPoints := make(map[string]bool)
+
+	for len(pointsToExplode) > 0 {
+		point := pointsToExplode[0]
+		pointsToExplode = pointsToExplode[1:]
+		if explodedPoints[point.ToString()] {
+			continue
+		}
+		explodedPoints[point.ToString()] = true
+		surroundingPoints := point.SurroundingPoints(true, width, height)
+		for _, surroundingPoint := range surroundingPoints {
+			x, y := surroundingPoint.X, surroundingPoint.Y
+			if matrix[y][x] > 0 {
+				matrix[y][x]++
+				if matrix[y][x] == 10 {
+					matrix[y][x] = 0
+					pointsToExplode = append(pointsToExplode, surroundingPoint)
+				}
+			}
+		}
+	}
+
+	explodedPointCount = len(explodedPoints)
+	return
 }
--- t/point.go
+++ t/point.go
@@ -15,6 +15,36 @@ func (p Point2D) ToString() string {
 	return strconv.Itoa(p.X) + ";" + strconv.Itoa(p.Y)
 }
 
+func (p Point2D) SurroundingPoints(includeDiagonal bool, matrixWidth int, matrixHeight int) []Point2D {
+	surroundingPoints := make([]Point2D, 0)
+
+	if p.X > 0 {
+		surroundingPoints = append(surroundingPoints, Point2D{X: p.X - 1, Y: p.Y})
+		if p.Y > 0 && includeDiagonal {
+			surroundingPoints = append(surroundingPoints, Point2D{X: p.X - 1, Y: p.Y - 1})
+		}
+		if p.Y < matrixHeight-1 && includeDiagonal {
+			surroundingPoints = append(surroundingPoints, Point2D{X: p.X - 1, Y: p.Y + 1})
+		}
+	}
+	if p.X < matrixWidth-1 {
+		surroundingPoints = append(surroundingPoints, Point2D{X: p.X + 1, Y: p.Y})
+		if p.Y > 0 && includeDiagonal {
+			surroundingPoints = append(surroundingPoints, Point2D{X: p.X + 1, Y: p.Y - 1})
+		}
+		if p.Y < matrixHeight-1 && includeDiagonal {
+			surroundingPoints = append(surroundingPoints, Point2D{X: p.X + 1, Y: p.Y + 1})
+		}
+	}
+	if p.Y > 0 {
+		surroundingPoints = append(surroundingPoints, Point2D{X: p.X, Y: p.Y - 1})
+	}
+	if p.Y < matrixHeight-1 {
+		surroundingPoints = append(surroundingPoints, Point2D{X: p.X, Y: p.Y + 1})
+	}
+	return surroundingPoints
+}
+
 func StringToPoint2D(s string) Point2D {
 	coords := utils.StringsToInts(strings.Split(s, ";"))
 	return Point2D{X: coords[0], Y: coords[1]}
