dcc7fbfa5f2090234a2dd544f72a0a88a28853fe
Medo <martinmederly@gmail.com>
2023-11-14 20:59:24 +0100

    feat: day15 basic setup

--- day15/day15.go
+++ day15/day15.go
@@ -1,5 +1,16 @@
 package day15
 
+import (
+	"github.com/the-medo/go-advent-2021/utils"
+)
+
 func Solve(input string) {
+	matrix, _ := utils.SplitToMatrix(input, false, false)
+
+	for row, matrixRow := range matrix {
+		for col, _ := range matrixRow {
+
+		}
+	}
 
 }
